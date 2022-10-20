3c0f94b1329dc5128f89c1f84adf88be5b4fdd90
Jainesh Pathak <c2063946@newcastle.ac.uk>
2022-10-20 16:05:01 +0100

    CmdInterface removed

--- CSC8501_Coursework/CmdInterface.h
+++ CSC8501_Coursework/CmdInterface.h
@@ -24,25 +24,6 @@ public:
 	void update();
 
 private:
-	struct strIndex;
-
-	void parsePoly(strIndex* strIndex, int str_size, Poly& poly);
-	int checkSign(strIndex* strIndex);
-	int checkExponent(strIndex* strIndex, bool& coeffIsConstant);
-
-	void askToContinue(int toState);
-	void askSaveFile(const std::vector<int>& outputSet);
-
 	void showMainMenu();
-	void processExpression();
-	void deriveExpression(const std::vector<int> outputSet);
-
-	void readFile(bool withExpression = false);
-	void saveSetToFile(const std::string& fileName, const std::vector<int>& outputSet);
-
-	void parseOutputSet(const std::vector<std::string>& outputLinesV);
-	void getOutputSet(const std::string& strSet);
+	void askToContinue(int toState);
 };
-
-void inputStringExpression(std::string& polyStr);
-void inputRangeAndCalc(int& start, int& end, Poly& poly, std::vector<int>& outputSet);
